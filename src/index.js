dfcc63baa7b339e223a1ba1f099e3977bc8a82cb
qus1225 <qus1225@gmail.com>
2018-02-07 00:27:22 +0900

    Set layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
+
+class Square extends React.Component {
+  render() {
+    return (
+        <button className="square">
+          {/*{this.props.value}*/}
+        </button>
+    )
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next palyer: X';
+
+    return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+        )
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+        <div className="game">
+          <div className="game-board">
+            <Board/>
+          </div>
+          <div className="game-info">
+            <div></div>
+            <ol></ol>
+          </div>
+        </div>
+    );
+  }
+}
+
+
+ReactDom.render(
+    <Game/>,
+    document.getElementById('root')
+);
